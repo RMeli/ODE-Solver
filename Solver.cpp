320540038d86d739c770a2722246a97792a21e28
Rocco Meli <rocco.meli@epfl.ch>
2014-11-09 11:03:55 +0100

    Solver class: apply the choosen integrator in order to solve the problem on the given interval.

---
+++ Solver.cpp
@@ -0,0 +1,47 @@
+#include "Solver.h"
+
+#include "Exception.h"
+
+Solver::Solver(double y0_, double xmin_, double xmax_, Integrator* I_)
+: y0(y0_), xmin(xmin_), xmax(xmax_), I_ptr(I_)
+{}
+
+void Solver::solve()
+{
+    double dx( I_ptr->get_dx() );
+    
+    double xn(xmin);
+    double yn(y0);
+    
+    std::array<double,2> step({0,0});
+    
+    while (xn < xmax)
+    {
+        step = {xn, yn};
+        solution.push_back(step);
+        
+        yn = I_ptr->step(xn,yn);
+        
+        xn += dx;
+    }
+}
+
+bool Solver::is_solved() const
+{
+    return ! solution.empty();
+}
+
+std::vector<std::array<double,2>> Solver::get_solution() const
+{
+    if (!is_solved())
+    {
+        throw Unsolved(); // Throw Unsolved exception
+    }
+    
+    return solution;
+}
+
+Solver::~Solver()
+{
+    delete I_ptr;
+}
