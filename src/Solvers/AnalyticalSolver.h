4534c45235a35c9cd4d68a5d46438ac3fe844b26
Rocco Meli <rocco.meli@epfl.ch>
2014-12-01 09:49:25 +0100

    Completed doxygen documentation

--- src/Solvers/AnalyticalSolver.h
+++ src/Solvers/AnalyticalSolver.h
@@ -9,18 +9,39 @@
 class AnalyticalSolver : public Solver
 {
 public:
+    //! Constructor.
+    /*!
+     Take the initial value y0=y(xmin), the interval of integration and an Integrator and the analytical solution.
+     */
     AnalyticalSolver(double y0, double xmin_, double xmax_, Integrator* I_, double (*s_)(double));
     
     AnalyticalSolver(const AnalyticalSolver&) = delete;
+    AnalyticalSolver(const AnalyticalSolver&&) = delete;
     AnalyticalSolver& operator=(const AnalyticalSolver&) = delete;
     
+    //! Solve.
+    /*!
+     Solve the problem and also evaluate the analytical solution at the same points of numerical evaluation.
+     */
     void solve();
     
+    //! Return analytical solution.
+    /*!
+     This function return the analytical_solution member. This member contains the analytical solution evaluated at the same points of numerical evaluation and contains also absolute and relative error.
+     */
     std::vector<std::array<double,3>> get_analytical_solution() const;
     
 private:
+    //! Analytical solution.
+    /*!
+     Pointer to the analytical solution (has to be provided).
+     */
     double (*s)(double);
     
+    //! Analytical solution.
+    /*!
+     This member contains the analytical solution evaluated at the same points of numerical evaluation and contains also absolute and relative error.
+     */
     std::vector<std::array<double,3>> analytical_solution;
 };
 
--- src/Solvers/Solver.h
+++ src/Solvers/Solver.h
@@ -10,6 +10,10 @@
 class Solver
 {
 public:
+    //! Constructor.
+    /*!
+     Take the initial value y0=y(xmin), the interval of integration and an Integrator.
+     */
     Solver(double y0, double xmin_, double xmax_, Integrator* I_);
     
     Solver(const Solver&) = delete;
@@ -22,19 +26,59 @@ public:
      */
     bool is_solved() const;
     
+    //! Solver function.
+    /*!
+     Use the Integrator to solve the problem, i.e. apply the integrator on the interval [xmin,xmax] and save the solution as points (xn,yn)
+     */
     void solve();
     
+    //! Get solution.
+    /*!
+     Return the solution as a vector of bi dimensional arrays. Each array contains the points (xn,yn)
+     */
     std::vector<std::array<double,2>> get_solution() const;
     
+    //! Get solution.
+    /*!
+     Print the solution as (xn,yn) inside a given ostream: this can be the cout as well as a data file.
+     */
     void print(std::ostream& out = std::cout) const;
     
+    //! Destructor.
+    /*!
+     Takes care of the deletion of the integrator.
+     */
     ~Solver();
     
 protected:
+    //! Solution.
+    /*!
+     Solution stored as a vector of bi-dimansional array corresponding to the points (xn,yn)
+     */
     std::vector<std::array<double,2>> solution;
+    
+    //! Initial condition.
+    /*!
+     Initial condition of the ODE: y0=y(xmin)
+     */
     double y0;
+    
+    //! x min
+    /*!
+     Lower bound of the integration interval.
+     */
     double xmin;
+    
+    //! x max
+    /*!
+     Upper bound of the integration interval.
+     */
     double xmax;
+    
+    //! Integrator
+    /*!
+     Integrator. The solver applay this integrator starting from the given initial condition on the interval [xmin,xmax].
+     */
     Integrator* I_ptr;
 };
 
